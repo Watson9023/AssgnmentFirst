4efd47ec8009a266e275170ff6a80f2dcbc96cef
Watson9023 <matunhirewendy@gmail.com>
2017-03-15 12:56:07 +0200

    First commit

---
+++ Testnew/Test/src/Testmain.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Watson on 3/15/2017.
+ */
+
+
+public class Testmain {
+
+    Package Watson90;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
